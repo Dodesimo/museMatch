b0620647b657545ef3ff2f0505385ff973782b48
devam <devammondal06@gmail.com>
2024-08-01 17:38:15 -0400

    figuring it out

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const recommendationRoutes = require('./api/recommendations/recommendations')
+const recommendationRoutes = require('./recommendations/recommendations')
 require('dotenv').config()
 const cors = require('cors')
 
