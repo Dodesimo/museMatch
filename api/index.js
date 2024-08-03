2796c9b3739e469b340ce1e3696521d626ae4353
devam <devammondal06@gmail.com>
2024-08-03 19:34:17 -0400

    authentication db added

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const recommendationRoutes = require('./recommendations/recommendations')
+const loginRoutes = require('./routes/user')
+const mongoose = require("mongoose")
 require('dotenv').config()
 const cors = require('cors')
 
@@ -23,6 +25,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/musematch', recommendationRoutes)
+app.use('/api/user', loginRoutes)
 
 //Routes
 app.get('/', (req, res) => {
@@ -31,11 +34,13 @@ app.get('/', (req, res) => {
 
 })
 
-app.listen(process.env.PORT, () => {
-
-
-    console.log('listening on port 4000')
-
-
-    }
-)
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
